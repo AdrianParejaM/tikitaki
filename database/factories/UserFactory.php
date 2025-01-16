52bf94f32fc8bd3a58223ac63abf3b84aae2e598
Adrian Pareja <adrianpareja.alu@iespacomolla.es>
2025-01-16 18:56:09 +0100

    Comenzada la DDBB

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'nickname' => fake()->unique()->userName(),
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'register_date' => fake()->date(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nickname')->unique();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('register_date');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
